adfa556d90dbad0247060f37fdd0dfa3a58e0f05
Nishith Khandwala <nishith@stanford.edu>
2014-11-23 21:49:15 -0800

    Now we have the feature vector

--- scripts/preprocessing.m
+++ scripts/preprocessing.m
@@ -1,6 +1,35 @@
-load '/Users/nishithkhandwala/Desktop/CS229_Project/data/CS229_speech_imagery.mat'
+load('/Users/nishithkhandwala/Desktop/CS229_Project/data/CS229_speech_imagery.mat');
 
 numBa = 80; numKu = 73; numIm = 178; numSi = 172;
+
+numChannels = 67; timeSpan = 1351;
+
+ba = zeros(numChannels, timeSpan);
+
+for indexFile = (101:180)
+    fileName = strcat('S',num2str(indexFile));
+    fileName = eval(fileName);
+    ba = ba + fileName.F;
+end
+
+ba = ba ./ numBa;
+
+startTime = 101; endTime = 1201;
+ba = ba(:, startTime:endTime);
+
+numParts = 8;
+
+[numRows, numCols] = size(ba);
+partSize = floor(numCols/numParts);
+
+partsArray = zeros(numChannels, numParts);
+
+for i = (1:numParts)
+   partsArray(:, i) = mean(ba(:, 1 + (i-1) * partSize : (i * partSize)), 2);
+end
+
+featureVector = reshape(partsArray, [1 numParts * numChannels]);
+
 
 
 
