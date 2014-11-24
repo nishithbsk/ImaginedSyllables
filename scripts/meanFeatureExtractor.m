ee27a651c6f5e1b4257046172e3dc733b8ad2444
Nishith Khandwala <nishith@stanford.edu>
2014-11-23 22:07:07 -0800

    Whoops

---
+++ scripts/meanFeatureExtractor.m
@@ -0,0 +1,6 @@
+baFeatureVector = obtainSyllableFeatureVector(101, 180);
+kuFeatureVector = obtainSyllableFeatureVector(201, 273);
+imFeatureVector = obtainSyllableFeatureVector(3001, 3178);
+siFeatureVector = obtainSyllableFeatureVector(4001, 4172);
+
+featureVector = [baFeatureVector; kuFeatureVector; imFeatureVector; siFeatureVector];
