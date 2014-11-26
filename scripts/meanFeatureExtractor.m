1ba8f7f446840be0a47b97f3b542216ff860b221
Nishith Khandwala <nishith@stanford.edu>
2014-11-26 00:39:05 -0800

    Reorganized code

--- scripts/meanFeatureExtractor.m
+++ scripts/meanFeatureExtractor.m
@@ -1,6 +1,20 @@
-baFeatureVector = obtainSyllableFeatureVector(101, 180);
-kuFeatureVector = obtainSyllableFeatureVector(201, 273);
-imFeatureVector = obtainSyllableFeatureVector(3001, 3178);
-siFeatureVector = obtainSyllableFeatureVector(4001, 4172);
+function meanFeatureVector = meanFeatureExtractor(syllable, numParts)
 
-featureVector = [baFeatureVector; kuFeatureVector; imFeatureVector; siFeatureVector];
+numChannels = 67;
+
+startTime = 101; endTime = 1201;
+syllable = syllable(:, startTime:endTime);
+
+[~, numCols] = size(syllable);
+partSize = floor(numCols/numParts);
+
+partsArray = zeros(numChannels, numParts);
+
+for i = (1:numParts)
+   partsArray(:, i) = mean(syllable(:, 1 + (i-1) * partSize : (i * partSize)), 2);
+end
+
+meanFeatureVector = reshape(partsArray, [1 numParts * numChannels]);
+
+end
+
