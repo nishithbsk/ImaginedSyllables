0cc21d9386118213243593be3ebe09b5fc4cc600
Nishith Khandwala <nishith@stanford.edu>
2014-11-28 04:20:08 -0800

    Whoops. Fixed a small bug. Does not affect results.

--- scripts/NN/NN.m
+++ scripts/NN/NN.m
@@ -6,13 +6,13 @@ targetVector = [];
 for i = 3001:3178
     featureVector = meanFeatureExtractor(getStructData(i), 8);
     inputMatrix = [inputMatrix; featureVector];
-    targetVector = [targetVector 0];
+    targetVector = [targetVector; 0];
 end
 
 for i = 4001:4172
     featureVector = meanFeatureExtractor(getStructData(i), 8);
     inputMatrix = [inputMatrix; featureVector];
-    targetVector = [targetVector 1];
+    targetVector = [targetVector; 1];
 end
 
 net = patternnet(6);
